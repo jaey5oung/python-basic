d96e0a96092990aa4e4a30d0b6d1868bae2e8b0c
장재영 <jaey5oung@naver.com>
2021-02-08 23:21:50 +0900

    list

--- test3.py
+++ test3.py
@@ -1,11 +1,19 @@
 list = []
 while True:
-    num =  input("입력하세요")
-    if num =="":
-        break;
-    else :
+    num = input("입력하세요")
+    if num == "":
+        break
+    else:
         list.append(int(num))
 list.sort()
 print(list)
 
 # 주말3문제풀기
+
+num = int(input())
+
+i = 1
+
+while i <= num:
+    print(i, i * i)
+    i = i + 1
